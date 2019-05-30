39746a254b0b4df7a8b1d2edb5be6985ab2b7ea2
oub9527 <81718917@qq.com>
2019-05-30 16:03:12 +0800

    添加自定义方法

---
+++ src/main/java/com/ob/business/repository/StudentRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.ob.business.repository;
+
+/**
+ * @Author: oubin
+ * @Date: 2019/5/30 13:47
+ * @Description:
+ */
+public interface StudentRepositoryCustom {
+
+    void addCount(String id);
+}
