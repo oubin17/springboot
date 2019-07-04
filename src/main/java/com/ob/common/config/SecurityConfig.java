37e8d3e226ea3762a64c7dbc3bd7a5c084975e95
oub9527 <81718917@qq.com>
2019-07-04 15:20:49 +0800

    添加spring boot security配置

---
+++ src/main/java/com/ob/common/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ob.common.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author: oubin
+ * @Date: 2019/6/20 14:06
+ * @Description:
+ */
+@Configurable
+@EnableWebSecurity
+@Order(101)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+}
